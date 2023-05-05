5cad1cacae95a52eaae3544a0e63b9c55147ffff
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-05-05 16:26:50 +0900

    Add files via upload

---
+++ ch05/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}></ScrollBox>
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
+}
+
+export default App;
