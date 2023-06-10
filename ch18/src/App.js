4f785816913e95b826403ba691f26ae6ee81f1af
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-06-11 00:55:32 +0900

    Add files via upload

---
+++ ch18/src/App.js
@@ -0,0 +1,12 @@
+//import SampleContainer  from "./containers/SampleContainer";
+import CounterContainer from "./containers/CounterContainer";
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer></CounterContainer>
+    </div>
+  );
+};
+
+export default App;
