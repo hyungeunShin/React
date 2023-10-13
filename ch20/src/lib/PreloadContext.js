17c1b3fe21654a0d66fc5f7b0292e2d6bb911254
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-10-13 21:58:52 +0900

    2023-10-13

--- ch20/src/lib/PreloadContext.js
+++ ch20/src/lib/PreloadContext.js
@@ -19,6 +19,9 @@ export const Preloader = ({ resolve }) => {
         return null;
     }
 
+    //promises 배열에 프로미스 등록
+    //설령 resolve 함수가 프로미스를 반환하지 않더라도 프로미스 취급을 하기 위해
+    //Promise.resolve() 사용
     preloadContext.promises.push(Promise.resolve(resolve()));
     return null;
 };
