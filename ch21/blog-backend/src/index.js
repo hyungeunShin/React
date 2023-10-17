bcd468206e37c27f7dba4d7c8d05b626eca56044
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-10-17 20:00:50 +0900

    2023-10-17

--- ch21/blog-backend/src/index.js
+++ ch21/blog-backend/src/index.js
@@ -1,28 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-    console.log(ctx.url);
-    console.log(1);
+//라우터 설정
+router.use('/api', api.routes());   //api 라우트 적용
 
-    if(ctx.query.authorized !== '1') {
-        ctx.status = 401;   //Unauthorized
-        return;
-    }
+//라우터 적용 전에 bodyParser적용
+app.use(bodyParser());
 
-    await next();
-    console.log('END');
-});
-
-app.use((ctx, next) => {
-    console.log(2);
-    next();
-});
-
-app.use(ctx => {
-    ctx.body = 'hello world';
-});
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('localhost:4000');
