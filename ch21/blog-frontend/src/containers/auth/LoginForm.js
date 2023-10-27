facd97ca22f7849cb6b3ccc34dc78d71e204c0f8
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-10-27 23:17:28 +0900

    2023-10-27

--- ch21/blog-frontend/src/containers/auth/LoginForm.js
+++ ch21/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,12 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { useNavigate } from 'react-router-dom';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.login
+    const navigate = useNavigate();
+
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+        form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user,
     }));
 
     //인풋 변경 이벤트 핸들러
@@ -24,6 +32,8 @@ const LoginForm = () => {
     //폼 등록 이벤트 핸들러
     const onSubmit = e => {
         e.preventDefault();
+        const { username, password } = form;
+        dispatch(login({ username, password }));
     };
 
     //컴포넌트가 처음 렌더링 될 때 form 을 초기화함
@@ -31,12 +41,33 @@ const LoginForm = () => {
         dispatch(initializeForm('login'));
     }, [dispatch]);
 
+    useEffect(() => {
+        if(authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            setError("로그인 실패");
+            return;
+        }
+
+        if(auth) {
+            console.log('로그인 성공');
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if(user) {
+            navigate("/");
+        }
+    }, [navigate, user]);
+
     return (
         <AuthForm
             type="login"
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
